922078c8e22766a2b8921c620c2cf9ea243ae965
perXautomatik <christoffer.broback@gmail.com>
2021-10-05 15:49:54 +0200

    error checking works, on exit opens output folder, filereading works

--- first.ps1
+++ first.ps1
@@ -1,6 +1,8 @@
 cd (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 . .\Forks\0cc040af9e7d768f13c998cde8dc414d\temporary-directory.ps1
 . .\Forks\7ca47b54d66abde42192471c53bbadcd\checking-for-null.ps1
+. .\Forks\fa4261bf1ff6e47734c2af4ec8c1f6a5\set-Encoding.ps1
+
 
 function GitCommitEach{
 
@@ -10,29 +12,27 @@ param
 [Parameter(ValueFromPipeline=$true)]
 $params
 )
-     
+   try{  
     if ($params.count -gt 0) {
     
         $folderPath = New-TemporaryDirectory
         cd $folderpath
         
         git init
-        
-        $params.count
-        $params.length
-        $params.GetType().Name
 
         #$params | %{
-        For ($i=0; $i -lt $params.length; $ii){
+        For ($i=0; $i -lt $params.length; $i++){
         $file = $params[$i]
         #$file = $_
     
             If( Test-Path -Path $file )
             {
             
-                Copy-Item $file -Destination $folderPath
-                      
-                $message = "$file.fullName $file.creationdate $file.modificationDate $file.size"
+                Copy-Item $file -Destination $folderPath -Force
+                $fileMeta = (Get-ChildItem $file)      
+                git add $fileMeta.Name
+
+                $message = '$fileMeta.FullName $file.CreationTime $file.LastWriteTime'
                 $message
 
                 git commit $message
@@ -43,11 +43,14 @@ $params
                 $file
             }
         }
-        ii $folderPath
-    
-    
+            
     }
-    else {
+        else {
+            
+        $params.count
+        $params.length
+        $params.GetType().Name
+
         "emptyParams"
         
         $params[0];
@@ -56,11 +59,28 @@ $params
         "<>"
         $params
         }
+
+        }
+        catch
+        {
+        $e 
+        $file
+        }
+
+        finally {        ii $folderPath}
+
+    
+
+
     
 }
 
+SetEncoding("UTF8")
 
 $csv = get-content -path "D:\Project Shelf\PowerShellProjectFolder\Todo\GeneralSourceCompare\fileList.txt" #-raw
 #$csv = import-csv "D:\Project Shelf\PowerShellProjectFolder\Todo\GeneralSourceCompare\fileList.txt"
-$csv | %{(get-item -literalpath $_)} | GitCommitEach 
+#$csv | %{ Test-Path -Path  $_ } 
+GitCommitEach($csv)
+
+#$csv | %{If( Test-Path -Path $_ ){$_}} 
 
