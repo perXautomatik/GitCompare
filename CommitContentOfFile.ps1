41a47c5dafbe0cb3cc02adfd12aa30dfa108b6ab
perXautomatik <christoffer.broback@gmail.com>
2022-10-24 10:10:46 +0200

    dunno

--- CommitContentOfFile.ps1
+++ CommitContentOfFile.ps1
@@ -1,24 +1,23 @@
-#loadDependensies
-
-Set-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+cd (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 . .\Forks\0cc040af9e7d768f13c998cde8dc414d\temporary-directory.ps1
 . .\Forks\7ca47b54d66abde42192471c53bbadcd\checking-for-null.ps1
 . .\Forks\fa4261bf1ff6e47734c2af4ec8c1f6a5\set-Encoding.ps1
 
-
+#todo, flagg, maintain filename, or mostcommon filename,or extension, or checksum
+#hashes as forks?, or paths as forks
 function GitCommitEach{
 
-    param
-    (
-    
-    [Parameter(ValueFromPipeline=$true)]
-    $params
-    )
+param
+(
+ 
+[Parameter(ValueFromPipeline=$true)]
+$params
+)
    try{  
     if ($params.count -gt 0) {
     
         $folderPath = New-TemporaryDirectory
-        Set-Location $folderpath
+        cd $folderpath
         
         git init
 
@@ -26,34 +25,56 @@ function GitCommitEach{
         For ($i=0; $i -lt $params.length; $i++){
         $file = $params[$i]
         #$file = $_
-        $tempName = "dest.txt"
-        
-        $destFileName = Join-Path -Path $folderPath -ChildPath $tempName
     
             If( Test-Path -Path $file )
             {
-            
-                Copy-Item $file -Destination $destFileName -Force 
-                $fileMeta = (Get-ChildItem $file)      
-                git add $tempName
-
-                $message = $fileMeta.Name + " "                 
-                $message = $message + $fileMeta.CreationTime  + " "
-                $message = $message + $fileMeta.LastWriteTime  + " "
-                $message = $message + $fileMeta.Parent + " " 
-                git commit -m $message 
-                try{
                 $hash = ""
+             try{
+                
                 $hash =  (Get-FileHash $file).hash
                 $hash = "$hash"      
                 
-                git tag -a $hash -m $i
+
                 }
                 catch
                 {
                  $hash
                     $hash.GetType().Name
                 }
+                $fileMeta = (Get-ChildItem $file)                  
+                
+            
+                         
+           $newDest = ($folderPath + '\' + $hash)
+           If( Test-Path -Path $newDest)
+            {
+                'exsists'
+            }
+            else
+            {
+            
+             try{
+              mkdir $newDest
+               } 
+             catch {$newDest}
+             
+
+            }
+
+            Copy-Item $file -Destination $newDest -Force
+
+           
+                             
+
+
+                git add ($hash + '\' + $fileMeta.Name)
+
+                $message = $fileMeta.FullName + " " 
+                $message = $message + $fileMeta.CreationTime  + " "
+                $message = $message + $fileMeta.LastWriteTime
+                git commit -m $message 
+                git tag -a $fileMeta.BaseName -m $i
+                
                 
             }
             else {
@@ -65,24 +86,24 @@ function GitCommitEach{
     }
         else {
             
-            $params.count
-            $params.length
-            $params.GetType().Name
+        $params.count
+        $params.length
+        $params.GetType().Name
 
-            "emptyParams"
+        "emptyParams"
         
-            $params[0];
+        $params[0];
         "<>"
-            $params[1]
+        $params[1]
         "<>"
-            $params
+        $params
         }
 
         }
         catch
         {
-            $e 
-            $file
+        $e 
+        $file
         }
 
         finally {        ii $folderPath}
@@ -95,8 +116,10 @@ function GitCommitEach{
 
 SetEncoding("UTF8")
 
-$csv = get-content -path "D:\Project Shelf\PowerShellProjectFolder\git\GeneralSourceCompare\fileList.txt" #-raw
-
+$csv = get-content -path "D:\Project Shelf\PowerShellProjectFolder\GeneralSourceCompare\fileList.txt" #-raw
+#$csv = import-csv "D:\Project Shelf\PowerShellProjectFolder\Todo\GeneralSourceCompare\fileList.txt"
+#$csv | %{ Test-Path -Path  $_ } 
 GitCommitEach($csv)
 
+#$csv | %{If( Test-Path -Path $_ ){$_}} 
 
